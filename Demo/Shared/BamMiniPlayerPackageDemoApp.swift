b11377c569d6fe73285c625a9424be6597b71d2e
Kaori <67104053+StudioKaori@users.noreply.github.com>
2022-09-10 10:09:36 +0200

    Add: Demo project

---
+++ Demo/Shared/BamMiniPlayerPackageDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BamMiniPlayerPackageDemoApp.swift
+//  Shared
+//
+//  Created by Kaori Persson on 2022-09-10.
+//
+
+import SwiftUI
+
+@main
+struct BamMiniPlayerPackageDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
