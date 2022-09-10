bd039cd7505b7697762f9ecf4b057cd7b1b2d342
Kaori <67104053+StudioKaori@users.noreply.github.com>
2022-09-10 11:10:31 +0200

    Add: assets and html there

--- Package.swift
+++ Package.swift
@@ -20,7 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftUIBamMiniPlayerLibrary",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources",
+            resources: [
+                .process("Resources/Media.xcassets")
+            ]),
         .testTarget(
             name: "SwiftUIBamMiniPlayerLibraryTests",
             dependencies: ["SwiftUIBamMiniPlayerLibrary"]),
