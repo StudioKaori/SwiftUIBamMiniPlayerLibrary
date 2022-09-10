28ccac134fdc8fb3cd135f5e17017ef85350c131
Kaori <67104053+StudioKaori@users.noreply.github.com>
2022-09-10 10:35:27 +0200

    Update PlayerStatus.swift

--- Sources/SwiftUIBamMiniPlayerLibrary/ObservableObjects/PlayerStatus.swift
+++ Sources/SwiftUIBamMiniPlayerLibrary/ObservableObjects/PlayerStatus.swift
@@ -10,9 +10,9 @@ import SwiftUI
 @available(iOS 13.0, *)
 public final class PlayerStatus: ObservableObject {
     // MARK: - Properties
-    @Published var isPlayerViewVisible: Bool = false
-    @Published var isPlayerMinimised: Bool = false
-    @Published var showID: String = ""
+    public @Published var isPlayerViewVisible: Bool = false
+    public @Published var isPlayerMinimised: Bool = false
+    public @Published var showID: String = ""
     
     // MARK: - Singleton instance
     public static let shared = PlayerStatus()
